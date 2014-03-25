1be7df5f78d53fd6d1bdbe50f39f76aa5c86d378
pw1316 <of19950402@gmail.com>
2014-03-25 17:24:45 +0800

    see what's wrong with the get/put image

--- Main.c
+++ Main.c
@@ -1,11 +1,18 @@
 #include<graphics.h>
 
-/*----------------------------------èƒŒæ™¯ç›¸å…³---------------------------------------*/
-void draw_background_paint(char *p,int text[]){			//ç”»åº•éƒ¨çš„å­— 
+int num[10][10],upalpha[26][10],lowalpha[26][10];		//Êý×Ö£¬×ÖÄ¸µÄµãÕó´æ´¢Êý×é 
+int screen=0;
+
+/**************************************************/
+/*ÔÚÆÁÄ»ÉÏ´òÓ¡Ò»¸ö×Ö·û                            */
+/*²ÎÊý£º p ´òÓ¡×Ö×óÉÏ½ÇµãµÄµØÖ·£¬text[] Òª´òÓ¡µÄ×Ö*/ 
+/*·µ»ØÖµ£ºÎÞ                                      */
+/**************************************************/ 
+void pwprint(char *p,int text[]){			//»­µ×²¿µÄ×Ö 
 	int i,j;
 	char *tmp;
-	for(i=0;i<8;i++){
-		for(j=4;j>0;j--){
+	for(i=0;i<10;i++){
+		for(j=7;j>0;j--){
 			if(text[i]&(1<<j)){
 				tmp=p+i*2*800+(4-j)*2;
 				*tmp=*(tmp+1)=*(tmp+800)=*(tmp+801)=0xFF;
@@ -14,33 +21,71 @@ void draw_background_paint(char *p,int text[]){			//ç”»åº•éƒ¨çš„å­—
 	}
 }
 
-void draw_background(){
+/**************************************************/
+/*ÉèÖÃ×Ö·ûµÄµãÕó                                  */
+/*²ÎÊý£ºÎÞ                                        */ 
+/*·µ»ØÖµ£ºÎÞ                                      */
+/**************************************************/ 
+void pwchar(){
+	num[0][0]=0x7E;
+	num[0][1]=0x46;
+	num[0][2]=num[0][3]=num[0][4]=0x4A;
+	num[0][5]=num[0][6]=num[0][7]=0x52;
+	num[0][8]=0x62;
+	num[0][9]=0x7E;
+
+} 
+
+/*----------------------------------±³¾°Ïà¹Ø---------------------------------------*/
+void pwbackground(){
 	int i;
 	char *p;
-	int text[20][8];					//F,H,e,l,p,E,x,i,t,S,a,v,A,n,d,1,9,0
-	text[0][0]=text[0][3]=0x1F;
-	text[0][1]=text[0][2]=text[0][4]=text[0][5]=text[0][6]=text[0][7]=0x10;
-	
-	text[15][0]=0x06;
-	text[15][1]=0x0E;
-	text[15][2]=text[15][3]=text[15][4]=text[15][5]=text[15][6]=0x02;
-	text[15][7]=0x0F;
-	//æœªå®Œå¾…ç»­
-	 
-	for(i=0;i<464000;i++){
+	for(i=0;i<462400;i++){
 		*(_vp+i)=BLUE;
+    }
+	for(i=462400;i<480000;i++){
+	    *(_vp+i)=0x1A;
 	}
-	for(i=464000;i<480000;i++){
-		*(_vp+i)=0x1A;
-	}
-	draw_background_paint(_vp+465602,text[0]);
+	pwprint(_vp+463205,num[0]);
+	pwprint(_vp+463221,num[0]);
 }
 /*----------------------------------------------------------------------------------*/ 
 
 
-/*----------------------------------ä¸»é¡µç›¸å…³----------------------------------------*/
-void draw_homepage(){
+/*----------------------------------Ö÷Ò³Ïà¹Ø----------------------------------------*/
+
+void pwhomepage(){
+	int key;
+	void *tmp_bar,*tmp_h_bar;
+	char *up=_vp+50*800+200;
+	char *down=_vp+200*800+200;
+	char *mid=up;
 	
+	setfillstyle(SOLID_FILL,RED);
+	bar(200,50,600,150);
+	getimage(200,50,600,150,tmp_h_bar);
+	setfillstyle(SOLID_FILL,0x1A);
+	bar(200,50,600,150);
+	bar(200,200,600,300);
+	getimage(200,50,600,150,tmp_bar);
+	
+	putimage(200,50,tmp_h_bar,COPY_PUT);
+	while(1){
+		if(bioskey(1)){
+			key=bioskey(0);
+		}
+		else continue;
+		if(key==_UP&&mid==down){
+			mid=up;
+			putimage(200,50,tmp_h_bar,COPY_PUT);
+			putimage(200,200,tmp_bar,COPY_PUT);
+		}
+		if(key==_DOWN&&mid==up){
+			mid=down;
+			putimage(200,50,tmp_bar,COPY_PUT);
+			putimage(200,200,tmp_h_bar,COPY_PUT);
+		}
+	}
 } 
 /*----------------------------------------------------------------------------------*/ 
 
@@ -48,6 +93,8 @@ void draw_homepage(){
 int main(){
 	int driver=DETECT,mode=VESA_800x600x8bit;
 	initgraph(&driver,&mode,"PW");
-	draw_background();
-	//return 0;
+	pwchar();
+	pwbackground();
+	pwhomepage();
+	return 0;
 }
