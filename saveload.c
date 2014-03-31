930ae4948e5733e1e587ec6a5b1bbddb81d86231
Key-Zhang <zmj1316@gmail.com>
2014-03-31 20:07:42 +0800

    补充注释

--- saveload.c
+++ saveload.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+//结构内容等待修改
 struct profile {
 		char name[10];
    		int num;
    		int score;
 	};
+//userid为用户编号，生成save%d.sav作为标记 | user为结构指针用于传递读取or写入的信息
 int readfile (int userid,struct profile *user){
 	FILE *fp;
 	struct profile reada[10];
@@ -24,6 +26,7 @@ void writefile (int userid, struct profile *user){
     fprintf(fp,"%s %d %d\n",user->name,user->num,user->score);
     fclose(fp);
 }
+//新建一个覆盖原来的user
 int newfile (int userid){
 	FILE *fp;
 	char a[10];
@@ -33,6 +36,15 @@ int newfile (int userid){
 	fprintf(fp, "%s %d %d\n",a,x,x);
 	fclose(fp);
 }
+
+
+
+
+
+
+
+
+//test 将test2写入文件 再读入test1中 再输出
 int testmain(){
 	char a[2];
 	struct profile test2={"test2",1,100},test1={"\0",0,0};
