390cc7a89aa33297054dc9ad9a8fe6df545f4fc8
Key-Zhang <zmj1316@gmail.com>
2014-03-31 19:57:18 +0800

    new file store one save file for each user

--- saveload.c
+++ saveload.c
@@ -8,48 +8,38 @@ int readfile (int userid,struct profile *user){
 	FILE *fp;
 	struct profile reada[10];
 	char c;
+	char a[10];
 	int n=0;
-	fp=fopen("save.dat","r");
-	if(fp==NULL) return 0;
-    	while(!feof(fp)) {
-       		fscanf(fp, "%s %d %d ", reada[n].name,&reada[n].num,&reada[n].score);
-       		n++;
-       		}
-	
+	sprintf(a,"save%d.sav",userid);
+	fp=fopen(a,"r");
+    fscanf(fp,"%s %d %d\n",user->name,&(user->num),&(user->score));
 	fclose(fp);
-	if (userid<=n)
-    	*user = reada[userid];
-    	else
-    		return 0;
-    	return 1;
 }
 void writefile (int userid, struct profile *user){
 	FILE *fp;
-	struct profile writea[11];
-	char c;
+	char a[10];
 	int n,i;
-	for(n=0;readfile(n,writea + n);n++);
-	writea[userid]=*user;
-	fp=fopen("save.dat","w");
-    	for(i=0; i < n-1; i++) 
-      	   fprintf(fp, "%s %d %d\n", writea[i].name,writea[i].num,writea[i].score);
-    	fclose(fp);
+	sprintf(a,"save%d.sav",userid);
+	fp=fopen(a,"w");
+    fprintf(fp,"%s %d %d\n",user->name,user->num,user->score);
+    fclose(fp);
 }
-int newfile (){
+int newfile (int userid){
 	FILE *fp;
-	fp=fopen("save.dat","w");
-	char a[]="id";
+	char a[10];
+	sprintf(a,"save%d.sav",userid);
+	fp=fopen(a,"w");
 	int x=0;
 	fprintf(fp, "%s %d %d\n",a,x,x);
 	fclose(fp);
 }
-int zmjtestmain(){
+int testmain(){
 	char a[2];
 	struct profile test2={"test2",1,100},test1={"\0",0,0};
 	struct profile *p,*q;
 	p=&test2;
 	q=&test1;
-	newfile();
+	newfile(0);
 	writefile(0,p);
 	readfile(0,q);
 	printf("%s %d %d\n",test1.name,test1.num,test1.score);
