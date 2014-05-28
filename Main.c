055c6764ce4841bb97e8641357af8a25f2e0cf61
Key-Zhang <zmj1316@gmail.com>
2014-05-28 18:59:07 +0800

    Signed-off-by: Key-Zhang <zmj1316@gmail.com>

--- Main.c
+++ Main.c
@@ -22,7 +22,9 @@ struct profile {
    		char age[3];
    		char height[4];
    		char weight[4];
-   		int recoder; // 记录饮食，可以：没吃-2 素-1 大素+0 荤+1 大荤+2  或者根据体重什么的加判断
+   		int recoder; // 记录次数 算平均
+   		double energy;
+   		double procnt;
 	};
 struct menu{
 	int recipeID;
@@ -30,15 +32,15 @@ struct menu{
 	int energy;//kcal
 	int procnt;//蛋白质？
 } load[100];
-
+struct profile* user;
 void readmenu();
 void pw_background();//画背景 
 void pw_homepage();//画主页
-
+void recode(int index);
 void pw_homebar3(); //show recipe
 void pw_homebar0();//My info +change
 void pw_homebar4();//change profile
-
+void pw_homebar2();
 struct profile* readfile (int userid);
 void newfile(int userid);
 
@@ -148,6 +150,7 @@ void pw_homepage(){
 				if(index==0) pw_homebar0();
 				if(index==4) pw_homebar4();
 				if(index==3) pw_homebar3();
+				if(index==2) pw_homebar2();
 			}//按下回车
 			else printf("%x\n",key);//测试按键的码 
 		}
@@ -155,7 +158,6 @@ void pw_homepage(){
 
 void pw_homebar0(){
 	void *old=malloc(800*600);
-	struct profile* user;
 	int line=220;
 	int key;
 	char a[50];
@@ -473,4 +475,100 @@ void readmenu(){
 	}//把所有菜品读取
 
 	fclose(fp);
+}
+//
+void pw_homebar2(){
+		struct Pos{
+		int x;
+		int y;
+	};//表示位置
+	int key;//按下的键
+	void *tmp_bar,*tmp_h_bar;//存储临时方块，getimage用
+	int index=0;//高亮方块编号
+	struct Pos pos[5]={{300,200},{300,280},{300,350},{300,420},{300,490}};//各方块位置
+	char *text[5]={"Not eat","Some vegs","Many vegs","Some meat","Lots of meat"};//各方块上打印的信息 
+	
+	/*分配getimage空间*/
+	tmp_bar=malloc(imagesize(pos[0].x,pos[0].y,pos[0].x+251-1,pos[0].y+BAR_HEIGHT-1));
+	tmp_h_bar=malloc(imagesize(pos[0].x,pos[0].y,pos[0].x+251-1,pos[0].y+BAR_HEIGHT-1));
+	
+	/*得到高亮方块*/
+	setfillstyle(SOLID_FILL,RED);
+	bar(pos[0].x,pos[0].y,pos[0].x+251-1,pos[0].y+BAR_HEIGHT-1);
+	getimage(pos[0].x,pos[0].y,pos[0].x+251-1,pos[0].y+BAR_HEIGHT-1,tmp_h_bar);
+	
+	/*得到低亮方块*/ 
+	setfillstyle(SOLID_FILL,DARKGRAY);
+	bar(pos[0].x,pos[0].y,pos[0].x+251-1,pos[0].y+BAR_HEIGHT-1);
+	getimage(pos[0].x,pos[0].y,pos[0].x+251-1,pos[0].y+BAR_HEIGHT-1,tmp_bar);
+	
+	/*画方块及字*/ 
+	putimage(pos[0].x,pos[0].y,tmp_h_bar,COPY_PUT);
+	putimage(pos[1].x,pos[1].y,tmp_bar,COPY_PUT);
+	putimage(pos[2].x,pos[2].y,tmp_bar,COPY_PUT);
+	putimage(pos[3].x,pos[3].y,tmp_bar,COPY_PUT);
+	putimage(pos[4].x,pos[4].y,tmp_bar,COPY_PUT);
+	setcolor(YELLOW);
+	outtextxy(pos[0].x+8,pos[0].y+BAR_HEIGHT*11/30,text[0]);
+	setcolor(WHITE);
+	outtextxy(pos[1].x+8,pos[1].y+BAR_HEIGHT*11/30,text[1]);
+	outtextxy(pos[2].x+8,pos[2].y+BAR_HEIGHT*11/30,text[2]);
+	outtextxy(pos[3].x+8,pos[3].y+BAR_HEIGHT*11/30,text[3]);
+	outtextxy(pos[4].x+8,pos[4].y+BAR_HEIGHT*11/30,text[4]);
+	
+	/*循环键盘输入*/ 
+	while(1)
+		if(bioskey(1)!=0){
+			key=bioskey(0);
+			if((key==UP||key==LEFT)&&index!=0){
+				putimage(pos[index].x,pos[index].y,tmp_bar,COPY_PUT);
+				setcolor(WHITE);
+				outtextxy(pos[index].x+8,pos[index].y+BAR_HEIGHT*11/30,text[index]);
+				index--;
+				putimage(pos[index].x,pos[index].y,tmp_h_bar,COPY_PUT);
+				setcolor(YELLOW);
+				outtextxy(pos[index].x+8,pos[index].y+BAR_HEIGHT*11/30,text[index]);
+			}//按下上键 
+			else if((key==DOWN||key==RIGHT)&&index!=4){
+				putimage(pos[index].x,pos[index].y,tmp_bar,COPY_PUT);
+				setcolor(WHITE);
+				outtextxy(pos[index].x+8,pos[index].y+BAR_HEIGHT*11/30,text[index]);
+				index++;
+				putimage(pos[index].x,pos[index].y,tmp_h_bar,COPY_PUT);
+				setcolor(YELLOW);
+				outtextxy(pos[index].x+8,pos[index].y+BAR_HEIGHT*11/30,text[index]);
+			}//按下下键 
+			else if(key==0x011B){
+				return;
+			}//按下ESC 
+			else if(key==ENTER){
+				recode(index);
+			}//按下回车
+			else printf("%x\n",key);//测试按键的码 
+		}
+}
+
+//注意！！这里读不到user数据（因为没有read）暂时懒得写
+void recode(int index){
+	int recoder=user->recoder;
+	int e=0,p=0;
+	user->recoder++;
+	if (index==1) {
+		e=500;
+		p=19;
+	}
+	else if (index==2){
+		e=700;
+		p=30;
+	}
+	else if (index==3){
+		e=1000;
+		p=40;
+	}
+	else {e=1500;p=60;}
+
+
+	user->energy=((user->energy * recoder) + e)/(recoder+1);
+	user->procnt=((user->procnt * recoder) + p)/(recoder+1);
+	return;
 }
--- recoder.c
+++ recoder.c
@@ -1,7 +1,88 @@
 //分析饮食
-//Male
-if (*user.gender=="M"){
-	if (user.recoder>=820&&user.recoder<=960) //正常，推荐900kcal的菜谱
-		//
-
+struct profile {
+		char name[11];
+   		char gender[2];
+   		char age[3];
+   		char height[4];
+   		char weight[4];
+   		int recoder; // 记录次数 算平均
+   		double energy;
+   		double procnt;
+	};
+//记录
+void pw_homebar2(){
+		struct Pos{
+		int x;
+		int y;
+	};//表示位置
+	int key;//按下的键
+	void *tmp_bar,*tmp_h_bar;//存储临时方块，getimage用
+	int index=0;//高亮方块编号
+	struct Pos pos[5]={{300,200},{300,280},{300,350},{300,420},{300,490}};//各方块位置
+	char *text[5]={"Not eat","Some vegs","Many vegs","Some meat","Lots of meat"};//各方块上打印的信息 
+	
+	/*分配getimage空间*/
+	tmp_bar=malloc(imagesize(pos[0].x,pos[0].y,pos[0].x+251-1,pos[0].y+BAR_HEIGHT-1));
+	tmp_h_bar=malloc(imagesize(pos[0].x,pos[0].y,pos[0].x+251-1,pos[0].y+BAR_HEIGHT-1));
+	
+	/*得到高亮方块*/
+	setfillstyle(SOLID_FILL,RED);
+	bar(pos[0].x,pos[0].y,pos[0].x+251-1,pos[0].y+BAR_HEIGHT-1);
+	getimage(pos[0].x,pos[0].y,pos[0].x+251-1,pos[0].y+BAR_HEIGHT-1,tmp_h_bar);
+	
+	/*得到低亮方块*/ 
+	setfillstyle(SOLID_FILL,DARKGRAY);
+	bar(pos[0].x,pos[0].y,pos[0].x+251-1,pos[0].y+BAR_HEIGHT-1);
+	getimage(pos[0].x,pos[0].y,pos[0].x+251-1,pos[0].y+BAR_HEIGHT-1,tmp_bar);
+	
+	/*画方块及字*/ 
+	putimage(pos[0].x,pos[0].y,tmp_h_bar,COPY_PUT);
+	putimage(pos[1].x,pos[1].y,tmp_bar,COPY_PUT);
+	putimage(pos[2].x,pos[2].y,tmp_bar,COPY_PUT);
+	putimage(pos[3].x,pos[3].y,tmp_bar,COPY_PUT);
+	putimage(pos[4].x,pos[4].y,tmp_bar,COPY_PUT);
+	setcolor(YELLOW);
+	outtextxy(pos[0].x+8,pos[0].y+BAR_HEIGHT*11/30,text[0]);
+	setcolor(WHITE);
+	outtextxy(pos[1].x+8,pos[1].y+BAR_HEIGHT*11/30,text[1]);
+	outtextxy(pos[2].x+8,pos[2].y+BAR_HEIGHT*11/30,text[2]);
+	outtextxy(pos[3].x+8,pos[3].y+BAR_HEIGHT*11/30,text[3]);
+	outtextxy(pos[4].x+8,pos[4].y+BAR_HEIGHT*11/30,text[4]);
+	
+	/*循环键盘输入*/ 
+	while(1)
+		if(bioskey(1)!=0){
+			key=bioskey(0);
+			if((key==UP||key==LEFT)&&index!=0){
+				putimage(pos[index].x,pos[index].y,tmp_bar,COPY_PUT);
+				setcolor(WHITE);
+				outtextxy(pos[index].x+8,pos[index].y+BAR_HEIGHT*11/30,text[index]);
+				index--;
+				putimage(pos[index].x,pos[index].y,tmp_h_bar,COPY_PUT);
+				setcolor(YELLOW);
+				outtextxy(pos[index].x+8,pos[index].y+BAR_HEIGHT*11/30,text[index]);
+			}//按下上键 
+			else if((key==DOWN||key==RIGHT)&&index!=4){
+				putimage(pos[index].x,pos[index].y,tmp_bar,COPY_PUT);
+				setcolor(WHITE);
+				outtextxy(pos[index].x+8,pos[index].y+BAR_HEIGHT*11/30,text[index]);
+				index++;
+				putimage(pos[index].x,pos[index].y,tmp_h_bar,COPY_PUT);
+				setcolor(YELLOW);
+				outtextxy(pos[index].x+8,pos[index].y+BAR_HEIGHT*11/30,text[index]);
+			}//按下下键 
+			else if(key==0x011B){
+				return;
+			}//按下ESC 
+			else if(key==ENTER){
+				if(index==0) pw_homebar0();
+				if(index==4) pw_homebar4();
+				if(index==3) pw_homebar3();
+			}//按下回车
+			else printf("%x\n",key);//测试按键的码 
+		}
 }
+}
+
+
+
